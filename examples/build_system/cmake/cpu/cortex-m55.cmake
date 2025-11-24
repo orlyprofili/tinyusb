29bae09fa6d50a32ca3737165d802b38802cf996
orlyprofili <orly@profilidesign.com>
2025-11-24 11:25:42 -0500

    modifications to cortex-m55.cmake.
    consider pulling file out of here and into project, and then report to upstream?  It's in an "examples" folder, as if I should copy and modify it for proj

--- examples/build_system/cmake/cpu/cortex-m55.cmake
+++ examples/build_system/cmake/cpu/cortex-m55.cmake
@@ -1,11 +1,19 @@
+if (NOT DEFINED CORTEX_M55_ENABLE_CMSE)
+  set(CORTEX_M55_ENABLE_CMSE OFF CACHE BOOL "Enable the CMSE instructions (-mcmse) for Cortex-M55 builds")
+endif ()
+
+set(_cmse_flag "-mcmse")
+
 if (TOOLCHAIN STREQUAL "gcc")
   set(TOOLCHAIN_COMMON_FLAGS
     -mthumb
     -mcpu=cortex-m55
     -mfloat-abi=hard
     #TODO: check this -mfpu=fpv5-d16
-    -mcmse
     )
+  if (CORTEX_M55_ENABLE_CMSE)
+    list(APPEND TOOLCHAIN_COMMON_FLAGS ${_cmse_flag})
+  endif ()
   set(FREERTOS_PORT GCC_ARM_CM55_NTZ_NONSECURE CACHE INTERNAL "")
 
 elseif (TOOLCHAIN STREQUAL "clang")
@@ -24,3 +32,24 @@ elseif (TOOLCHAIN STREQUAL "iar")
   set(FREERTOS_PORT IAR_ARM_CM55_NTZ_NONSECURE CACHE INTERNAL "")
 
 endif ()
+
+foreach(_lang IN ITEMS C CXX ASM)
+  if (DEFINED CMAKE_${_lang}_FLAGS)
+    if (CORTEX_M55_ENABLE_CMSE)
+      if (CMAKE_${_lang}_FLAGS MATCHES "-mcmse")
+        continue()
+      endif ()
+      set(_new_flags "${CMAKE_${_lang}_FLAGS} ${_cmse_flag}")
+      string(STRIP "${_new_flags}" _new_flags)
+      set(CMAKE_${_lang}_FLAGS "${_new_flags}" CACHE STRING "" FORCE)
+    else()
+      if (NOT CMAKE_${_lang}_FLAGS MATCHES "-mcmse")
+        continue()
+      endif ()
+      string(REPLACE "-mcmse" "" _new_flags "${CMAKE_${_lang}_FLAGS}")
+      string(REGEX REPLACE "[ ]+" " " _new_flags "${_new_flags}")
+      string(STRIP "${_new_flags}" _new_flags)
+      set(CMAKE_${_lang}_FLAGS "${_new_flags}" CACHE STRING "" FORCE)
+    endif ()
+  endif ()
+endforeach()
