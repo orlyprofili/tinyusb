d30213920c8a1ff1b357f440c562a5f93d02951f
orlyprofili <orly@profilidesign.com>
2025-11-24 19:54:15 -0500

    custom STM32N657XX_RAM_ONLY.ld linker

--- hw/bsp/stm32n6/boards/stm32n657nucleo/board.cmake
+++ hw/bsp/stm32n6/boards/stm32n657nucleo/board.cmake
@@ -1,7 +1,10 @@
 set(MCU_VARIANT stm32n657xx)
 set(JLINK_DEVICE stm32n6xx)
 
-set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/STM32N657XX_AXISRAM2_fsbl.ld)
+# Use our custom linker script that utilizes full 4.2MB RAM
+# Located in project root's linker/ directory
+# Path: stm32n657nucleo -> boards -> stm32n6 -> bsp -> hw -> tinyusb -> project_root
+set(LD_FILE_GNU ${CMAKE_CURRENT_LIST_DIR}/../../../../../../linker/STM32N657XX_RAM_ONLY.ld)
 
 function(update_board TARGET)
   target_compile_definitions(${TARGET} PUBLIC
